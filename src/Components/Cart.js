580525187e386efecd63a9afe5c8a4ddbe0999a8
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-01-23 22:05:22 +0200

    btn

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -31,7 +31,15 @@ const Cart =()=> {
      <td>{item.title}</td>
      <td>{item.price}</td>
      <td>Quantity: ({item.quantity})</td>
-     <td></td>    
+     <td>
+     <button 
+     className='btn btn-info ms-2' 
+     onClick={()=> updateItemQuantity(item.id, item.quantity -1)}>-</button>
+      <button 
+      className='btn btn-info ms-2'
+      onClick={()=> updateItemQuantity(item.id, item.quantity +1)}>+</button>
+      <button className='btn btn-danger ms-2'>Remove Item</button>
+     </td>    
      </tr>
      )
   })}
