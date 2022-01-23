51ac577115e135e1cfd914ccc328e1db487316f9
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-01-23 22:19:01 +0200

    btn2

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -38,13 +38,25 @@ const Cart =()=> {
       <button 
       className='btn btn-info ms-2'
       onClick={()=> updateItemQuantity(item.id, item.quantity +1)}>+</button>
-      <button className='btn btn-danger ms-2'>Remove Item</button>
+      <button 
+      className='btn btn-danger ms-2'
+      onClick={()=> removeItem(item.id)}
+      >Remove Item</button>
      </td>    
      </tr>
      )
   })}
   </tbody>
   </table>
+  </div>
+  <div className='col-auto ms-auto'>
+  <h2>Total Price: $ {cartTotal}</h2>
+  </div>
+  <div className='col-auto'>
+  <button 
+  className='btn btn-danger m-2'
+  onClick={()=>emptyCart()}
+  >Clear Cart</button>
   </div>
   </div>
 </section>
